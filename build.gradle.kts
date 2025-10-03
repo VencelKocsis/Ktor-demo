4d2ef060c4c7ee96ba5b07f6c82802e622742a48
VencelKocsis <katzarmondi@gmail.com>
2025-10-03 14:05:02 +0200

    Deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     alias(libs.plugins.kotlin.plugin.serialization)
+
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hu.bme.aut.android.demo"
@@ -11,6 +13,14 @@ application {
     mainClass.set("hu.bme.aut.android.demo.ApplicationKt")
 }
 
+// Konfiguráció a shadowJar pluginhoz, biztosítva, hogy a fő osztály fusson
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    archiveClassifier.set("all") // A létrehozott JAR neve: projekt-all.jar
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+}
+
 dependencies {
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.core)
@@ -18,14 +28,17 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
 
-    // Új függőségek hozzáadva
+    // WebSocket támogatás
+    implementation(libs.ktor.server.websockets)
+
+    // Adatbázis (HikariCP + Exposed + PostgreSQL)
     implementation(libs.exposed.core)
     implementation(libs.exposed.dao)
     implementation(libs.exposed.jdbc)
     implementation(libs.hikaricp)
     implementation(libs.postgresql)
-    implementation(libs.ktor.server.websockets)
 
+    // Teszt függőségek
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
